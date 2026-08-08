51ad047b181a61662bd47b4a7a5aed4f018ceaeb
bkane522-lab <bkane522@gmail.com>
2026-08-08 19:03:42 +0200

    Add files via upload

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,12 +1,7 @@
-import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
-export default function PublicLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
