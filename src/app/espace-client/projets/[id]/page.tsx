91bc1047ed953af0e55f2abc5d228902e28ef922
bkane522-lab <bkane522@gmail.com>
2026-08-10 19:28:38 +0200

    Add files via upload

--- src/app/espace-client/projets/[id]/page.tsx
+++ src/app/espace-client/projets/[id]/page.tsx
@@ -1,1 +1,6 @@
+import { ProjectDetail } from "@/components/ProjectDetail";
 
+export default async function ProjectDetailPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  return <ProjectDetail projectId={id} />;
+}
