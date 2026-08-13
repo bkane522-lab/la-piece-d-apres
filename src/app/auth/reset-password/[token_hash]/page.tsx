0a64d3b0aba2f77946e7fae8d1793b559fec45de
bkane522-lab <bkane522@gmail.com>
2026-08-13 23:41:09 +0200

    Delete src/app/auth/reset-password directory

--- src/app/auth/reset-password/[token_hash]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { ConfirmReset } from "@/components/ConfirmReset";
-
-export default async function ResetPasswordConfirmPage({ params }: { params: Promise<{ token_hash: string }> }) {
-  const { token_hash } = await params;
-  return <ConfirmReset tokenHash={token_hash} />;
-}
